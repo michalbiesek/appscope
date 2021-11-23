b06263fc703e3a2e6a9f2b877b30259f93418320
michalbiesek <mbiesek@cribl.io>
2021-11-23 17:04:31 +0100

    Revert "Extend Test Runner with TCP Listener"
    This reverts commit 6dd3b3c57a705422623449a77576250730fa64b2.

--- test/testContainers/test_runner/runner.py
+++ test/testContainers/test_runner/runner.py
@@ -101,7 +101,7 @@ class Runner:
                 if data:
                     logging.info(data)
 
-            self.__collector.wait()
+            time.sleep(1)
             scope_messages = self.__collector.get_all()
             logging.info(f"Received {len(scope_messages)} messages from scope")
             if len(scope_messages) > 0: logging.debug(f"Last 10 messages:\n {''.join(scope_messages[-9:])}")
