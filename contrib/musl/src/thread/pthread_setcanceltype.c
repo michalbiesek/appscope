f521898e88f186e4642e053054aff719f27ec923
michalbiesek <mbiesek@cribl.io>
2022-03-30 19:52:20 +0200

    Comment logic in pthread_setcanceltype

--- contrib/musl/src/thread/pthread_setcanceltype.c
+++ contrib/musl/src/thread/pthread_setcanceltype.c
@@ -4,8 +4,8 @@ int pthread_setcanceltype(int new, int *old)
 {
 	struct pthread *self = __pthread_self();
 	if (new > 1U) return EINVAL;
-	if (old) *old = self->cancelasync;
-	self->cancelasync = new;
-	if (new) pthread_testcancel();
+	// if (old) *old = self->cancelasync;
+	// self->cancelasync = new;
+	// if (new) pthread_testcancel();
 	return 0;
 }
