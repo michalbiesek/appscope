3c7ab23c4f6f27bf3f48ddfed83bd451ba362f80
Donn <donn@cribl.io>
2022-04-14 17:55:53 +0000

    WIP: resolved a segfault in Go tlsServer. Updated LOCK to use the actual thread ID.

--- src/wrap.c
+++ src/wrap.c
@@ -1046,6 +1046,11 @@ periodic(void *arg)
     perf = checkEnv(PRESERVE_PERF_REPORTING, "true");
 
     while (1) {
+        // we are trying to exit, do nothing
+        if (g_exitdone == TRUE) {
+            while (1) pthread_yield();
+        }
+
         scope_gettimeofday(&tv, NULL);
         if (tv.tv_sec >= summaryTime) {
             // Process dynamic config changes, if any
