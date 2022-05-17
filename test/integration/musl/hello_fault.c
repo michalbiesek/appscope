e75f94ed6590be143ee85ce0111a1dbdcc86f0e0
michalbiesek <mbiesek@cribl.io>
2022-05-17 14:37:24 +0200

    [WIP] Extend fault test with `SIGBUS`

--- test/integration/musl/hello_fault.c
+++ test/integration/musl/hello_fault.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define READ_ONLY_VAR 0
+#define READ_ONLY_VAR  0
 #define NOT_MAPPED_VAR 1
+#define BUS_ERROR_VAR  2
 
 void read_only_area_error(void) {
     char *s = "Hello goat";
@@ -14,6 +15,18 @@ void not_mapped_address_error(void) {
    fprintf(stderr, "%s", test);
 }
 
+void bus_error(void) {
+# if defined(__x86_64__)
+    __asm__("pushf\norl $0x40000,(%rsp)\npopf");
+# endif
+       short *sptr;
+       int    i;
+
+       sptr = (short *)&i;
+       sptr = (short *)(((char *)sptr) + 1);
+       *sptr = 100;
+}
+
 void test_function(int variant) {
    switch (variant){
       case READ_ONLY_VAR:
@@ -22,8 +35,10 @@ void test_function(int variant) {
       case NOT_MAPPED_VAR:
          not_mapped_address_error();
          break;
+      case BUS_ERROR_VAR:
+         bus_error();
+         break;
       default:
-         // not expected reach here
          exit(EXIT_SUCCESS);
          break;
    }
@@ -33,7 +48,6 @@ int main(int argc, char *argv[])
 {
    int variant;
    if (argc != 2) {
-      // not expected reach here
       return 0;
    }
    variant = atoi(argv[1]);
