b313b8c013e5b9a0a4258f69bc2c0f93dcdcf298
michalbiesek <mbiesek@cribl.io>
2022-08-04 17:16:27 +0200

    Add `lseek` and `ftruncate` to internal library

--- src/scopestdlib.c
+++ src/scopestdlib.c
@@ -55,6 +55,7 @@ extern int            scopelibc_access(const char *, int);
 extern FILE*          scopelibc_fmemopen(void *, size_t, const char *);
 extern long           scopelibc_ftell(FILE *);
 extern int            scopelibc_fseek(FILE *, long, int);
+extern off_t          scopelibc_lseek(int, off_t, int);
 extern int            scopelibc_unlink(const char *);
 extern int            scopelibc_dup2(int, int);
 extern char*          scopelibc_basename(char *);
@@ -203,6 +204,7 @@ extern int           scopelibc_shmget(key_t, size_t, int);
 extern int           scopelibc_sched_getcpu(void);
 extern int           scopelibc_rand(void);
 extern void          scopelibc_srand(unsigned int);
+extern int           scopelibc_ftruncate(int, off_t);
 extern int           scopelibc_setns(int, int);
 
 static int g_go_static;
@@ -430,6 +432,11 @@ scope_ftell(FILE *stream) {
 int
 scope_fseek(FILE *stream, long offset, int whence) {
     return scopelibc_fseek(stream, offset, whence);
+}
+
+off_t
+scope_lseek(int fd, off_t offset, int whence) {
+    return scopelibc_lseek(fd, offset, whence);
 }
 
 int
@@ -1137,6 +1144,11 @@ scope_shmget(key_t key, size_t size, int shmflg) {
 int
 scope_sched_getcpu(void) {
     return scopelibc_sched_getcpu();
+}
+
+int
+scope_ftruncate(int fildes, off_t length) {
+    return scopelibc_ftruncate(fildes, length);
 }
 
 int
