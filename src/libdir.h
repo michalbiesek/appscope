caae34f7fd5837115ef0508dcfd1ca4768997041
michalbiesek <mbiesek@cribl.io>
2022-10-07 09:15:05 +0200

    Remove arg name from declaration

--- src/libdir.h
+++ src/libdir.h
@@ -11,8 +11,8 @@
 #define LIBRARY_FILE 0 // libscope.so
 #define LOADER_FILE 1  // ldscopedyn
 
-int         libdirSetBase(int file, const char *base);    // Override default base dir i.e. /tmp
-int         libdirExtract(int file);                      // Extracts file to default path
-const char* libdirGetPath(int file);                      // Get full path to existing file 
+int         libdirSetBase(int, const char *);    // Override default base dir i.e. /tmp
+int         libdirExtract(int);                  // Extracts file to default path
+const char* libdirGetPath(int);                  // Get full path to existing file 
 
 #endif // _SCOPE_LIBDIR_H
