843402efabf6e22870860b964e149725b9d75d20
michalbiesek <mbiesek@cribl.io>
2023-01-17 15:52:36 +0100

    Add IPC test

--- cli/ipc/ipc_test.go
+++ cli/ipc/ipc_test.go
@@ -1,10 +1,39 @@
 package ipc
 
 import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func newIpcTest() (*ipcObj, error) {
+	sender, err := newMsgQWriter(fmt.Sprintf("ScopeIPCIn.%d", os.Getpid()))
+	if err != nil {
+		return nil, errCreateMsgQWriter
+	}
+
+	receiver, err := newMsgQReader(fmt.Sprintf("ScopeIPCOut.%d", os.Getpid()))
+	if err != nil {
+		sender.destroy()
+		return nil, errCreateMsgQReader
+	}
+	return &ipcObj{sender: sender, receiver: receiver, ipcSame: true, mqUniqId: 1234}, nil
+}
+
+func (ipc *ipcObj) sendTestDataToReader(msg []byte) error {
+	mqfd, err := msgQOpen(ipc.sender.name, unix.O_WRONLY|unix.O_NONBLOCK)
+	if err != nil {
+		return err
+	}
+	err = msgQSend(mqfd, msg)
+	if err != nil {
+		return err
+	}
+	return unix.Close(mqfd)
+}
 
 func TestMsgSeparatorMissing(t *testing.T) {
 	sep := ipcGetMsgSeparatorIndex([]byte("test"))
@@ -69,5 +98,11 @@ func TestParsingIpcFrameScopeMsgMissingStatus(t *testing.T) {
 }
 
 func TestParsingIpcFrameScopeMsgMultiple(t *testing.T) {
-
+	ipc, err := newIpcTest()
+	assert.NoError(t, err)
+	defer ipc.destroyIPC()
+	testMsg := "{\"status\":200,\"uniq\":1645,\"remain\":28}\x00{\"scoped\":true}"
+	err = ipc.sendTestDataToReader([]byte(testMsg))
+	assert.NoError(t, err)
+	ipc.receiveIpcResponse()
 }
