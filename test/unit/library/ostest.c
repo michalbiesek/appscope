ae8efa4d8d467a5f97afc9a960005f397fa1e636
michalbiesek <mbiesek@cribl.io>
2023-06-22 18:55:11 +0200

    Revert the changes related to seperate code to `sig.c`

--- test/unit/library/ostest.c
+++ test/unit/library/ostest.c
@@ -1,8 +1,33 @@
 #define _GNU_SOURCE
 
+#include "fn.h"
 #include "os.h"
 #include "scopestdlib.h"
 #include "test.h"
+
+static void
+dummyHandler(int sig, siginfo_t *info, void *secret) {
+    assert_int_equal(sig, SIGUSR2);
+    assert_int_equal(info->si_signo, SIGUSR2);
+    assert_int_equal(info->si_code, SI_TIMER);
+    // copy pasta from SIG_ORIGIN_SCOPE_ID
+    assert_int_equal(info->si_int, 123456789);
+}
+
+static void
+osTestTimerStopNotInit(void **state) {
+    bool res = osTimerStop();
+    assert_false(res);
+}
+
+static void
+osTestTimerBasic(void **state) {
+    bool res = osThreadInit(dummyHandler, 1);
+    assert_true(res);
+    scope_sleep(2);
+    res = osTimerStop();
+    assert_true(res);
+}
 
 static void
 osWritePermSuccess(void **state) {
@@ -36,7 +61,11 @@ int
 main(int argc, char* argv[]) {
     printf("running %s\n", argv[0]);
 
+    initFn();
+
     const struct CMUnitTest tests[] = {
+        cmocka_unit_test(osTestTimerStopNotInit),
+        cmocka_unit_test(osTestTimerBasic),
         cmocka_unit_test(osWritePermSuccess),
         cmocka_unit_test(osWritePermFailure),
         cmocka_unit_test(dbgHasNoUnexpectedFailures),
